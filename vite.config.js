00fe0eed9c39d6061af5a642b0e5f908074ce086
Prassanth <ap.138@outlook.com>
2024-11-29 08:56:37 +0530

    vite: updated for unix

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true, // Important for Windows file system
-      interval: 2000,
-    },
-  },
 });
